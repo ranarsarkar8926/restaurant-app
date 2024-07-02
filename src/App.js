24599dcf59c1a9c6d8f2f06a61bad539f34adccf
ranarsarkar8926 <136478055+ranarsarkar8926@users.noreply.github.com>
2024-07-03 02:20:38 +0530

    my restaurant

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant.js";
+function App() {
+  return (     
+    <>
+      <Resturant/>
+    </>
+  );
+}
+export default App;
